b42714e134265e2b5f4a67f058a056f9b17d708d
Saravanan-2006 <saravananb583223104097@nprcolleges.org>
2024-10-16 10:31:04 +0530

    Add files via upload

---
+++ mohith.py
@@ -0,0 +1,18 @@
+def binary_search(arr,x):low=0
+high+len(arr)
+low<=high
+
+mid+(high+low)// 2 if
+arr [mid]x:
+    low=mid+1
+def arr[mid]>x:
+    high=mid-1
+    else:
+        return mid
+    return-1
+    arr=[23,4,10,40]
+    x=40
+    result!=-1:
+    print("element is present at index"str (result))
+    else:
+    print("element is not present in array")
